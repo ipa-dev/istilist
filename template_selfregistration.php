50f0691425001bd1d5a697081a3c9276bfbe8305
Mason Watson <mkw@Masons-MacBook-Pro.local>
2018-07-27 13:38:58 -0400

    styling

--- template_selfregistration.php
+++ template_selfregistration.php
@@ -8,7 +8,8 @@
 	<div class="maincontent noPadding">
 	    <div class="section group">
 	        <?php //get_sidebar('menu'); ?>
-	        <div class="col span_9_of_12 matchheight">
+          <div class="col span_2_of_12"></div>
+	        <div class="col span_8_of_12 matchheight">
                 <div class="dash_content">
                   <?php
                     if(isset($_POST['add_new_shopper'])){
@@ -332,6 +333,7 @@
                   </form>
                 </div>
           </div>
+          <div class="col span_2_of_12"></div>
           <?php get_footer(); ?>
       </div>
   </div>
