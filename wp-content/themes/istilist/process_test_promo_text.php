ef0dcd4ec308f82bd5a6468ede827f2ef8ea5eec
Mason Watson <mkw@Masons-MacBook-Pro-5.local>
2018-12-27 16:09:13 -0500

    change directory

--- wp-content/themes/istilist/process_test_promo_text.php
+++ wp-content/themes/istilist/process_test_promo_text.php
@@ -1,6 +1,6 @@
 <?php /* Template Name: Test Promo Text */ ?>
 <?php
-    require("/home/istilist/public_html/wp-content/themes/standard-theme/twilio-php-master/Twilio/autoload.php");
+    require("/home3/istilist/public_html/wp-content/themes/standard-theme/twilio-php-master/Twilio/autoload.php");
     use Twilio\Rest\Client;
 
     $body = get_user_meta(get_post_meta(get_the_ID(), 'store_id', true), 'daily_promo_text', true);
@@ -9,19 +9,16 @@
         $token = '1859b70bd4b570f6c8ff702b1ffd005d';
         $client = new Client($sid, $token);
         $sms = $client->account->messages->create(
-
-            // the number we are sending to - Any phone number
+            // the number we are sending to
             '+1'.get_user_meta(get_post_meta(get_the_ID(), 'store_id', true), 'mobile_number', true),
                             
             array(
-                // Step 6: Change the 'From' number below to be a valid Twilio number
-                // that you've purchased
                 'from' => get_option('twilio_number'),
                 // the sms body
                 'body' => $body
             )
         );
     }
-    header("Location: http://istilist.com/store-profile/");
+    header("Location: https://istilist.com/store-profile/");
     
 ?>
