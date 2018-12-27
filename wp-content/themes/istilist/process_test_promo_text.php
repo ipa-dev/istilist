f6532c5477954cd60fb66922fd04d9df518a20ee
Mason Watson <mkw@Masons-MacBook-Pro-5.local>
2018-12-27 16:25:53 -0500

    debugging tests

--- wp-content/themes/istilist/process_test_promo_text.php
+++ wp-content/themes/istilist/process_test_promo_text.php
@@ -3,14 +3,16 @@
     require("/home3/istilist/public_html/wp-content/themes/istilist/twilio-php-master/Twilio/autoload.php");
     use Twilio\Rest\Client;
 
-    $body = get_user_meta(get_post_meta(get_the_ID(), 'store_id', true), 'daily_promo_text', true);
+    global $user_ID;
+    $body = get_user_meta($user_ID, 'daily_promo_text', true);
     if (trim(strtolower($body)) != 'na') {
         $sid = 'ACdb92d82faf7befbb1538a208224133a4';
         $token = '1859b70bd4b570f6c8ff702b1ffd005d';
         $client = new Client($sid, $token);
+        echo get_user_meta($user_ID, 'mobile_number', true);
         $sms = $client->account->messages->create(
             // the number we are sending to
-            '+1'.get_user_meta(get_post_meta(get_the_ID(), 'store_id', true), 'mobile_number', true),
+            '+1'.get_user_meta($user_ID, 'mobile_number', true),
                             
             array(
                 'from' => get_option('twilio_number'),
