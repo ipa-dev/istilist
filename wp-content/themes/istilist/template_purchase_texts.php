235e5c6acb66439e7e35a96d3290855e6ee68ac0
Mason Watson <mkw@Masons-MacBook-Pro-5.local>
2018-12-28 13:09:47 -0500

    add box to purchase-text page

--- wp-content/themes/istilist/template_purchase_texts.php
+++ wp-content/themes/istilist/template_purchase_texts.php
@@ -5,7 +5,16 @@
 ?>
 <div id="dashboard">
 	<div class="maincontent noPadding">
-        
+        <div class="section group">
+            <?php get_sidebar('menu'); ?>
+            <div class="col span_9_of_12 matchheight">
+                <div class="dash_content">
+                    <h1><?php the_title(); ?></h1>
+                    <div class="box">
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <?php } ?>
