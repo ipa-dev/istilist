14b669758e2f71e1a9ad2ebf1a21a3f60ce60c20
Mason Watson <mkw@Masons-MacBook-Pro-5.local>
2018-12-28 12:39:30 -0500

    beginning modules

---
+++ wp-content/themes/istilist/php_modules/store-profile-profile-picture.php
@@ -0,0 +1,18 @@
+<div class="col span_6_of_12">
+    <label>Profile Picture ON/OFF</label>
+    <input type="radio" name="profile_pic_on_off" value="1" <?php if (get_user_meta($user_ID, 'profile_pic_on_off', true) == 1) {
+                echo 'checked="checked"';
+            } ?> /> ON
+    <input type="radio" name="profile_pic_on_off" value="0" <?php if (get_user_meta($user_ID, 'profile_pic_on_off', true) == 0) {
+                echo 'checked="checked"';
+            } ?> /> OFF
+</div>
+<div class="col span_6_of_12 matchheight">
+<?php if (get_user_meta($user_ID, 'profile_pic_on_off', true) == 1) {
+                ?>
+    <label>Profile Picture</label>
+    <input type="file" name="profile_pic" />
+    <?php //echo get_store_img($user_ID);?>
+<?php
+            } ?>
+</div>
