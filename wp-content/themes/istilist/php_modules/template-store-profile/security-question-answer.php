32faba7f017492952dba6bac8af2c05c83c23255
Mason Watson <mkw@Masons-MacBook-Pro-5.local>
2018-12-28 12:58:07 -0500

    more modules

---
+++ wp-content/themes/istilist/php_modules/template-store-profile/security-question-answer.php
@@ -0,0 +1,4 @@
+<div class="col span_6_of_12">
+    <label>Your answer</label>
+    <input type="text" name="security_answer" value="<?php echo get_user_meta($user_ID, 'security_answer', true); ?>" />
+</div>
---
+++ wp-content/themes/istilist/php_modules/template-store-profile/security-question.php
@@ -0,0 +1,10 @@
+<div class="col span_6_of_12">
+    <label>Security question</label>
+    <select name="security_questions">
+        <option value="What was your childhood nickname?">What was your childhood nickname?</option>
+        <option value="What is the name of your favorite childhood friend?">What is the name of your favorite childhood friend?</option>
+        <option value="What street did you live on in third grade?">What street did you live on in third grade?</option>
+        <option value="What school did you attend for sixth grade?">What school did you attend for sixth grade?</option>
+        <option value="In what city or town was your first job?">In what city or town was your first job?</option>
+    </select>
+</div>
