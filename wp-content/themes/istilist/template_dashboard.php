cda77a1b938fd6b5ff8e25e64674f3f6a757be12
Mason Watson <mkw@Masons-MacBook-Pro-5.local>
2019-01-03 16:34:31 -0500

    modularize stylist-popup

--- wp-content/themes/istilist/template_dashboard.php
+++ wp-content/themes/istilist/template_dashboard.php
@@ -1,12 +1,9 @@
 <?php /* Template Name: Dashboard */ ?>
 <?php get_header(); ?>
 <?php if (is_user_logged_in()) {
-    ?>
-<?php global $user_ID;
-    global $wpdb; ?>
-<?php $store_id = get_user_meta($user_ID, 'store_id', true); ?>
-<div id="stylistpopup">
-	<?php
+    global $user_ID;
+    global $wpdb; 
+    $store_id = get_user_meta($user_ID, 'store_id', true);
     if (isset($_POST['bulk_select'])) {
         if ($_POST['bulk_select'] == 'all-shoppers') {
             global $user_ID;
@@ -183,47 +180,9 @@
                 }
             }
         }
-    } ?>
-    <form method="post" action="<?= get_bloginfo('url'); ?>/process-assign-stylist">
-        <div class="section group">
-            <div class="col span_12_of_12">
-                <h3>Assign Stylist</h3>
-                <label>Select Stylist</label>
-                <select name="stylist_id">
-					<?php
-                    $user_query = new WP_User_Query(array(
-                        'role'       => 'storeemployee',
-                        'meta_key'   => 'store_id',
-                        'meta_value' => $store_id,
-                        'orderby'    => 'display_name',
-                        'order'      => 'ASC'
-                    ));
-    if (! empty($user_query->results)) {
-        foreach ($user_query->results as $user) {
-            $user_status = $wpdb->get_results($wpdb->prepare("SELECT * FROM $wpdb->users WHERE ID = %s", $user->ID));
-            if ($user_status[0]->user_status != 0) {
-                echo '<option value="' . $user->ID . '">' . $user->display_name . '</option>';
-            }
-        }
-    } ?>
-                </select>
-
-            </div>
-        </div>
-        <div class="section group">
-            <div class="col span_12_of_12">
-                <label>Fitting Room ID</label>
-                <input type="number" pattern="[0-9]*" inputmode="numeric" name="fitting_room_id"/>
-            </div>
-        </div>
-        <div class="section group">
-            <div class="col span_12_of_12">
-                <input id="shopper_id" type="hidden" name="shopper_id" value=""/>
-                <input type="submit" name="assign_stylist" value="Assign"/>
-            </div>
-        </div>
-    </form>
-</div>
+    }
+    require_once 'php_modules/template-dashboard/stylist-popup.php';
+?>
 <div id="dashboard">
     <div class="maincontent noPadding">
         <div class="section group">
