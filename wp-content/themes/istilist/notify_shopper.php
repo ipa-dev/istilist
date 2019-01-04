b9b5930bdb248356eaee2ec51e8a493cb95b8cb2
Mason Watson <mkw@Masons-MBP-5.netgear.com>
2019-01-03 19:22:45 -0500

    work on twilio error

--- wp-content/themes/istilist/notify_shopper.php
+++ wp-content/themes/istilist/notify_shopper.php
@@ -23,7 +23,7 @@
             'body' => "Hey, ".get_post_meta($_POST['shopperID'], 'customer_fname', true).", your fitting room at ".get_user_meta(get_post_meta($_POST['shopperID'], 'store_id', true), 'store_name', true)." is now available."
         )
         );
-        if ($sms->error_code == '21610') { //error code for blacklisted number
+        if ($sms->code == '21610') { //error code for blacklisted number
             echo "na";
         }
         else {
